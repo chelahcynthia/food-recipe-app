13070c140b38eb3fef373c757ba2de4f30ee9ddb
cynthia <cchelangat9892gmail.com>
2022-09-04 12:25:34 +0300

    Add index js to index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// event listeners.....
+searchBtn.addEventListener('click', getMealList);
+
+
+// get meal list that matches the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    // console.log(searchInputTxt.length);
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
+    .then
+}
