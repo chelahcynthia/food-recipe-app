046890466fa9ef0cdd85f2f744ffb3148ab63cc3
cynthia <cchelangat9892gmail.com>
2022-09-04 13:27:38 +0300

    Add searchInputTxt changes on index,js

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ function getMealList(){
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-    })
+        // console.log(data);
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html +=  `
+                <div class = "meal-item" id = "${meal.idMeal}>
+                <div class = "meal-img">
+                  <img src = "${meal.strMealThumb}" alt="food">
+                    </div>
+                    <div class = "meal-name">
+                      <h3>${meal.strMeal}</h3>
+                      <a href="#" class = "recipe-btn">Get Recipe</a>
+                    </div>
+                   
+
+              </div>
+                `;
+            })
+        }
+        mealList.innerHTML = html;
+    });
 }
